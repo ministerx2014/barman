2e242df0fd9a63dcbbda1758787ae1be53d20a3b
unknown <baraks@BarakS-desk.Fourier.israel>
2014-01-07 10:39:59 +0200

    new fix

--- EW/js/default.js
+++ EW/js/default.js
@@ -67,7 +67,7 @@ ewActivityManager.slideManager = {
         
         /* TODO: Convert this code into my objects...
          * 
-         */
+        
         var maxSpeed = {car:300, bike:60, motorbike:200, airplane:1000,
                     helicopter:400, rocket:8*60*60}
                 var sortable = [];
